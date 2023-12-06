fa19ddfb575ecfd6da1a93e1fed129674b57b316
anishavvip <anishavvip@gmail.com>
2023-12-06 06:48:15 +0400

    .

--- push.js
+++ push.js
@@ -1,6 +1,8 @@
 //Your OneSignal Code Here
 window.OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
+  OneSignal.SERVICE_WORKER_PARAM = { scope: "/mbaye/" };
+  OneSignal.SERVICE_WORKER_PATH = "mbaye/OneSignalSDKWorker.js";
   OneSignal.init({
     appId: "835258e4-e7b5-4c3b-a87f-b65121118dbb",
     safari_web_id: "web.onesignal.auto.4dbe0dd2-36c1-4474-980b-740086f7dd0e",
@@ -11,6 +13,5 @@ OneSignal.push(function () {
     },
     persistNotification: true,
     autoResubscribe: true,
-    path: "https://cdn.onesignal.com/sdks/OneSignalSDK.js",
   });
 });
