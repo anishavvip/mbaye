7feb047fdde976afba4c81beffbf8cb9bf8c8015
anishavvip <anishavvip@gmail.com>
2023-12-06 06:25:05 +0400

    .

--- push.js
+++ push.js
@@ -1,6 +1,8 @@
 //Your OneSignal Code Here
 window.OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
+  OneSignal.SERVICE_WORKER_PARAM = { scope: "/push/" };
+  OneSignal.SERVICE_WORKER_PATH = "push/OneSignalSDKWorker.js";
   OneSignal.init({
     appId: "835258e4-e7b5-4c3b-a87f-b65121118dbb",
     safari_web_id: "web.onesignal.auto.4dbe0dd2-36c1-4474-980b-740086f7dd0e",
