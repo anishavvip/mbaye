85eb9ed259268ec995c6a46af3df5df83a206ea5
anishavvip <anishavvip@gmail.com>
2023-12-06 06:12:23 +0400

    .

---
+++ push.js
@@ -0,0 +1,15 @@
+//Your OneSignal Code Here
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function () {
+  OneSignal.init({
+    appId: "835258e4-e7b5-4c3b-a87f-b65121118dbb",
+    safari_web_id: "web.onesignal.auto.4dbe0dd2-36c1-4474-980b-740086f7dd0e",
+    notifyButton: {
+      enable: true,
+      size: "small",
+      position: "bottom-left",
+    },
+    allowLocalhostAsSecureOrigin: true,
+    subdomainName: "mbaye12",
+  });
+});
